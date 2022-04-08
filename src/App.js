40bfd63ca928194a71265edee4ddebbe614e8948
AnastasssiaK <kulynychnastya@gmail.com>
2022-04-08 14:32:24 +0300

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import css from './App.module.css'
+import {SingleUser, Users} from "./components";
+import {useState} from "react";
+import {Form1} from "./components/Form1/Form1";
+import {Form2} from "./components/Form2/Form2";
+
 const App = () => {
-  return (
-      <div>
-        App
-      </div>
-  );
-}
+    const [singleUser, setSingleUser] = useState(null);
+    return (
+        <div>
+            {/*{singleUser && <SingleUser user={singleUser} />}*/}
+            {/*<Users getUser={setSingleUser}/>*/}
+            {/*<Form1/>*/}
+            <Form2/>
+        </div>
+    );
+};
 
 export default App;
