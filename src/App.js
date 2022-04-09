4567cf576c5ed1c6721d33d20bb1e100be041763
AnastasssiaK <kulynychnastya@gmail.com>
2022-04-10 00:09:56 +0300

    homework 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import {Comments, Posts, Users} from "./components";
+import './App.css';
+
 const App = () => {
-  return (
-      <div>
-        App
-      </div>
-  );
+    return (
+        <div className={'wrap'}>
+            <div className={'users_posts'}>
+                <Users/>
+                <Posts/>
+            </div>
+            <Comments/>
+        </div>
+    );
 }
 
 export default App;
