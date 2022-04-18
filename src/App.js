f2c48d78ad596dcf37c4d411c3bfc15b824cc04e
AnastasssiaK <kulynychnastya@gmail.com>
2022-04-18 13:21:22 +0300

    hw 4 resolve

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import './App.css';
+import { MainLayout } from "./layouts";
+import { AboutPage, HomePage, NotFoundPage, PostsPage, SinglePostPage, SingleUserPage, UsersPage } from "./pages";
+
 const App = () => {
-  return (
-      <div>
-        App
-      </div>
-  );
-}
+    return (
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route index element={<Navigate to={'home'}/>}/>
+                <Route path={'home'} element={<HomePage/>}/>
+                <Route path={'users'} element={<UsersPage/>}>
+                    <Route path={':userId'} element={<SingleUserPage/>}>
+                        <Route path={'posts'} element={<PostsPage/>}/>
+                    </Route>
+                </Route>
+                <Route path={'posts'} element={<PostsPage/>}>
+                    <Route path={':id'} element={<SinglePostPage/>}/>
+                </Route>
+                <Route path={'about'} element={<AboutPage/>}/>
+                <Route path={'*'} element={<NotFoundPage/>}/>
+            </Route>
+        </Routes>
+    );
+};
 
 export default App;
