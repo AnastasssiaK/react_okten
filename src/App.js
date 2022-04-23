dcbc9378f2740ac847e9872a70a46577020d65da
Анастасія Кулинич <kulynychnastya@gmail.com>
2022-04-23 11:09:03 +0300

    lesson 5 part1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import {Routes, Route, Navigate} from "react-router-dom";
+import {MainLayout} from "./layouts";
+import {AboutPage, HomePage, LoginPage, NotFoundPage, PostsPage, SinglePostPage, UsersPage} from "./pages";
+import {RequireAuth} from "./hoc/RequireAuth";
+
 const App = () => {
-  return (
-      <div>
-        App
-      </div>
-  );
-}
+    return (
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route index element={<Navigate to={'home'}/>}/>
+                <Route path={'home'} element={<HomePage/>}/>
+                <Route path={'users'} element={
+                    <RequireAuth>
+                        <UsersPage/>
+                    </RequireAuth>
+                }/>
+                <Route path={'posts'} element={<PostsPage/>}>
+                    <Route path={':id'} element={<SinglePostPage/>}/>
+                </Route>
+                <Route path={'about'} element={<AboutPage/>}/>
+                <Route path={'login'} element={<LoginPage/>}/>
+                <Route path={'*'} element={<NotFoundPage/>}/>
+            </Route>
+        </Routes>
+    );
+};
 
 export default App;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,3 +1,4 @@
+
 import {Button} from "../Button/Button";
 import {Link, useNavigate} from "react-router-dom";
 import css from "../Button/Button.module.css";
